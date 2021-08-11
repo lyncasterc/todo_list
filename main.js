0e19e174a9eb23bbb73f78f864510ed8f164ce69
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-11 01:05:14 -0400

    refactor todoItems array to work with local storage

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('input-button');
 const itemsList = document.getElementById('items-list');
-const todoItems = [];
+let todoItems = JSON.parse(localStorage.getItem('items')) || [];
+localStorage.setItem('items', JSON.stringify(todoItems));
 
 const findTodoItem = function(todoText){
     return todoItems.find(item => item.todo === todoText );
